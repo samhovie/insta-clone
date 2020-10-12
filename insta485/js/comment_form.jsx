de64c0e1b54a1488b2be7b07929869691e704fd8
Unser Hashsham <unserh@umich.edu>
2020-10-11 20:15:34 -0400

    Add Comments React component

---
+++ insta485/js/comment_form.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class CommentForm extends React.Component {
+  /** Form for submitting comments on post
+   *
+   * Props:
+   * - updateComments callback to update comment state
+   *
+   * State:
+   * - value of the text in the form
+   */
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    const { updateComments } = this.props;
+    const { value } = this.state;
+    updateComments(value);
+    this.setState({
+      value: '',
+    });
+    event.preventDefault();
+  }
+
+  render() {
+    const { value } = this.state;
+    return (
+      <form
+        className="comment-form"
+        onSubmit={(event) => this.handleSubmit(event)}
+      >
+        <input
+          type="text"
+          value={value}
+          onChange={(event) => this.handleChange(event)}
+        />
+        <input type="submit" />
+      </form>
+    );
+  }
+}
+
+CommentForm.propTypes = {
+  updateComments: PropTypes.func.isRequired,
+};
+
+export default CommentForm;
--- insta485/js/comments.jsx
+++ insta485/js/comments.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import CommentForm from './comment_form';
 
 class Comments extends React.Component {
   /* Display number of likes and like/unlike button for one post
-   * 
+   *
    * Props:
    * - url for api endpoint for comment data
-   * 
+   *
    * State:
-   * - comments - list of dicts 
+   * - comments - list of dicts
    */
 
   constructor(props) {
@@ -20,9 +21,9 @@ class Comments extends React.Component {
           comment_owner: '',
           comment_owner_page_url: '',
           comment_text: '',
-        }
+        },
       ],
-    }
+    };
     this.updateComments = this.updateComments.bind(this);
   }
 
@@ -30,156 +31,94 @@ class Comments extends React.Component {
     const { url } = this.props;
 
     fetch(url, { credentials: 'same-origin', method: 'GET' })
-    .then((response) => {
-      if (!response.ok) throw Error(response.statusText);
-      return response.json();
-    }).then((data) => {
-      // Transform JSON data into list of dicts with relevant data
-      let comments = [];
-      data.comments.forEach((datum) => {
-        let comment = {
-          comment_id: datum.commentid,
-          comment_owner: datum.owner,
-          comment_owner_page_url: datum.owner_show_url,
-          comment_text: datum.text,
-        }
-        comments.push(comment);
-      });
+      .then((response) => {
+        if (!response.ok) throw Error(response.statusText);
+        return response.json();
+      }).then((data) => {
+        // Transform JSON data into list of dicts with relevant data
+        const comments = [];
+        data.comments.forEach((datum) => {
+          const comment = {
+            comment_id: datum.commentid,
+            comment_owner: datum.owner,
+            comment_owner_page_url: datum.owner_show_url,
+            comment_text: datum.text,
+          };
+          comments.push(comment);
+        });
 
-      this.setState({
-        comments: comments,
-      });
-    })
-    .catch((error) => console.log(error));
+        this.setState({ comments });
+      })
+      .catch((error) => console.log(error));
   }
 
-  // comment_data should be json parsed response of POST request to
-  // /api/v1/p/<post_id>/comments/
-  updateComments(comment_data) {
-    let comments  = this.state.comments.slice();
-    comments.push({
-      comment_id: comment_data.commentid,
-      comment_owner: comment_data.owner,
-      comment_owner_page_url: comment_data.owner_show_url,
-      comment_text: comment_data.text,
-    });
+  updateComments(value) {
+    // If submission is empty, don't do anything
+    if (value === '') {
+      return;
+    }
 
-    this.setState({
-      comments: comments,
-    });
+    const { url } = this.props;
+    const body = {
+      text: value,
+    };
+
+    // Post the comment
+    fetch(
+      url,
+      {
+        credentials: 'same-origin',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      },
+    )
+      .then((response) => {
+        if (!response.ok) throw Error(response.statusText);
+        return response.json();
+      })
+      .then((data) => {
+        this.setState((prevState) => {
+          const { comments } = prevState;
+          comments.push({
+            comment_id: data.commentid,
+            comment_owner: data.owner,
+            comment_owner_page_url: data.owner_show_url,
+            comment_text: data.text,
+          });
+          return ({ comments });
+        });
+      })
+      .catch((error) => console.log(error));
   }
 
   render() {
     const { url } = this.props;
     const { comments } = this.state;
-    const comment_items = comments.map((comment_data) => {
-      return (
-        <li key={comment_data.comment_id}>
-          <a href={comment_data.comment_owner_page_url}> {comment_data.comment_owner} </a>
-          <p> {comment_data.comment_text} </p>
-        </li>
-      );
-    });
+    const commentItems = comments.map((commentData) => (
+      <li key={commentData.comment_id}>
+        <a href={commentData.comment_owner_page_url}>
+          {commentData.comment_owner}
+        </a>
+        <p>
+          {commentData.comment_text}
+        </p>
+      </li>
+    ));
 
     return (
       <div className="comments">
-        <ul>{comment_items}</ul>
+        <ul>{commentItems}</ul>
         <CommentForm url={url} updateComments={this.updateComments} />
       </div>
-    )
-  }
-}
-
-
-class CommentForm extends React.Component {
-  /** Form for submitting comments on post
-   * 
-   * Props:
-   * - url for api endpoint to submit comment
-   * - callback to update comment state
-   * 
-   * State:
-   * - value of the text in the form
-   */
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: '',
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({
-      value: event.target.value,
-    });
-  }
-
-  handleSubmit(event) {
-    // If submission is empty, don't do anything
-    if (this.state.value === '') {
-      return;
-    }
-    
-    const { url } = this.props;
-    const body = {
-      text: this.state.value,
-    }
-
-    // Post the comment
-    fetch(
-      url, 
-      {
-        credentials: 'same-origin',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(body),
-      }
-    )
-    .then((response) => {
-      if (!response.ok) throw Error(response.statusText);
-      return response.json();
-    })
-    .then((data) => {
-      // Update comments with data in post response
-      this.props.updateComments(data);
-    })
-    .catch((error) => console.log(error));
-
-    // After comment is submitted, clear form
-    this.setState({
-      value: '',
-    });
-
-    event.preventDefault();
-  }
-
-  render() {
-    const { value } = this.state;
-    return (
-      <form className="comment-form" onSubmit={(event) => this.handleSubmit(event)}>
-        <input
-          type="text"
-          value={value}
-          onChange={(event) => this.handleChange(event)}
-        />
-        <input type="submit"></input>
-      </form>
     );
   }
 }
 
 Comments.propTypes = {
   url: PropTypes.string.isRequired,
-};
-
-CommentForm.propTypes = {
-  url: PropTypes.string.isRequired,
-  updateComments: PropTypes.func.isRequired,
 };
 
 export default Comments;
